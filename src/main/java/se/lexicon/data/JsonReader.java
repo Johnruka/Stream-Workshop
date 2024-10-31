56fd634af24ba1969eef514118894b86f7af49f3
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-10-31 18:33:34 +0100

    Modified

--- src/main/java/se/lexicon/data/JsonReader.java
+++ src/main/java/se/lexicon/data/JsonReader.java
@@ -1,11 +1,13 @@
 package se.lexicon.data;
 
 
-import jdk.internal.org.objectweb.asm.TypeReference;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import se.lexicon.model.Person;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +28,11 @@ public class JsonReader {
     public List<Person> read(){
         List<Person> people = new ArrayList<>();
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
         File file = new File("resources/people.json");
         try{
             people = objectMapper.readValue(file, new TypeReference<List<Person>>() {});
+
+
         }catch (IOException ex){
             System.out.println(ex + " " + ex.getMessage());
         }
--- src/main/java/se/lexicon/service/PeopleImpl.java
+++ src/main/java/se/lexicon/service/PeopleImpl.java
@@ -1,6 +1,6 @@
 package se.lexicon.service;
 
-import se.lexicon.JsonReader;
+import se.lexicon.data.JsonReader;
 import se.lexicon.model.Person;
 
 import java.util.List;
@@ -19,9 +19,11 @@ public class PeopleImpl implements People {
 
     private List<Person> people;
 
-    private void PeopleImpl() {
+    private PeopleImpl(){
         people = JsonReader.getInstance().read();
-    }
+
+
+}
 
     @Override
     public List<Person> getPeople() {
